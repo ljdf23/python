3b0b2157da1bc01ef7c52778a3ebf67ae9bad23a
Schizophrenia <schizophrenia_@hotmail.com>
2018-01-10 06:29:35 -0600

    testing de ciclos

--- Test_1/Ciclos.py
+++ Test_1/Ciclos.py
@@ -5,3 +5,15 @@ while manzanas > 0:
     manzanas-=1
  
 print("Me quede sin manzanas")
+
+lista_numeros = [1,2,3,4,5,6,7,8,9,10]
+
+for numero in lista_numeros:    
+    if numero == 3:
+        continue
+    print(numero)
+
+vocales = "aeiou"
+ 
+for vocal in vocales:
+    print(vocal.upper())
